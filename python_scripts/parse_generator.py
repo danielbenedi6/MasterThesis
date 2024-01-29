4b0fe6e84e150a727240a28515b92d7a81cb6b3b
Daniel Benedí <danielbenedigarcia@gmail.com>
2024-01-29 19:39:58 +0100

    Move plotting scripts

--- python_scripts/parse_generator.py
+++ python_scripts/parse_generator.py
@@ -1,6 +1,14 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas
+import matplotlib
+matplotlib.use("pgf")
+matplotlib.rcParams.update({
+    "pgf.texsystem": "pdflatex",
+    'font.family': 'serif',
+    'text.usetex': True,
+    'pgf.rcfonts': False,
+})
 
 filename = input("Enter the file path csv to analyze: ")
 
@@ -10,9 +18,10 @@ data["microseconds"] /= data["N"]
 plt.figure(figsize=(15,10))
 sns.lineplot(data, x="N", y="microseconds", hue="reader")
 plt.xlabel("Number of lines")
-plt.ylabel("Time per line [μs]")
+plt.ylabel("Time per line [$\mu$s]")
 #plt.yscale("log")
-plt.title("Time comparison of reading file (N = 100)")
-plt.savefig(filename+".png", dpi=900)
+plt.title("Time comparison of reading file")
+plt.savefig("figures/" + filename.split(".")[0].split("/")[-1]+".pgf")
+
 
 print(data[data["N"] >= 100000].groupby(["reader"]).mean())
