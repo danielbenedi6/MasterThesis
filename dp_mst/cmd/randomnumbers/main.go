abab858a53a8bada17ea70848a3330c79d8aa714
Daniel Benedí <danielbenedigarcia@gmail.com>
2024-02-05 19:04:41 +0100

    Chi-Squared performed

--- dp_mst/cmd/randomnumbers/main.go
+++ dp_mst/cmd/randomnumbers/main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	//"slices"
+	"os"
+
 	"time"
 
 	"gonum.org/v1/gonum/stat/distuv"
@@ -19,9 +20,20 @@ func main() {
 
 	fmt.Scanf("%d", &N)
 
-	k := int(N / 5)
+	k := int(N/5) - 1
 
 	Δb := 1.0 / float64(k)
+
+	f, err := os.Create("random_numbers.txt")
+	if err != nil {
+		panic(err)
+	}
+	// close fo on exit and check for its returned error
+	defer func() {
+		if err := f.Close(); err != nil {
+			panic(err)
+		}
+	}()
 
 	C := make([]int, k)
 	//V := make([]float64, N)
@@ -30,8 +42,27 @@ func main() {
 
 		C[int(elem/Δb)]++
 		//V[i] = elem
+		//fmt.Fprintln(f, V[i])
+		fmt.Fprintln(f, elem)
 	}
-	//slices.Sort(V)
+	//sort.Float64s(V)
+
+	//maxKminus := 0.0
+	//maxKplus := 0.0
+	//for i := 0; i < N; i++ {
+	//	if maxKplus < (float64(i)/float64(N) - V[i]) {
+	//		maxKplus = float64(i)/float64(N) - V[i]
+	//	}
+	//
+	//	if maxKminus < (V[i] - float64(i-1)/float64(N)) {
+	//		maxKminus = V[i] - float64(i-1)/float64(N)
+	//	}
+	//}
+	//maxKminus *= math.Sqrt(float64(N))
+	//maxKplus *= math.Sqrt(float64(N))
+	//
+	//fmt.Println("K⁺ = ", maxKplus)
+	//fmt.Println("K⁻ = ", maxKminus)
 
 	χ2 := 0.0
 	for j := 0; j < k; j++ {
